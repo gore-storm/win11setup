f2760c162209118d58b48cb09bfae6fdb3128198
gore-storm <117521498+gore-storm@users.noreply.github.com>
2022-11-14 23:00:45 +1100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy -Scope CurrentUser Bypass
 #update windows
 #update store apps
 
@@ -29,20 +30,31 @@ winget install -e --id Microsoft.VisualStudioCode -h --accept-package-agreements
 #winget install -e --id VMware.WorkstationPlayer -h --accept-package-agreements --accept-source-agreements;
 #winget install -e --id JAMSoftware.TreeSize.Free -h --accept-package-agreements --accept-source-agreements;
 #winget install -e --id Git.Git --accept-package-agreements -h --accept-source-agreements;
+winget install JanDeDobbeleer.OhMyPosh
 
-#Remove apps
+  #Remove apps
 winget uninstall  -e --id Microsoft.Teams
 
-#Remove Search, Task View, Chat and News And Interests from taskbar
+  #Remove Search, Task View, Chat and News And Interests from taskbar
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name SearchBoxTaskbarMode -Value 0 -Type DWord -Force;
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name ShowTaskViewButton -Value 0 -Type DWord -Force;
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarDa -Value 0 -Type DWord -Force;
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarMn -Value 0 -Type DWord -Force;
 
-#Align taskbar to left
+  #Align taskbar to left
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarAl -Value 0 -Type DWord -Force;
 
 # move .\settings.json to:
 #%LocalAppData%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 
 # install .\jjtheme.deskthemepack (timeout /t 3; taskkill /im "systemsettings.exe" /f)
+
+Install-Module PSReadLine -AllowPrerelease -Force
+New-Item -Path $PROFILE -Type File -Force
+notepad $profle
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+  
+  . $PROFILE
